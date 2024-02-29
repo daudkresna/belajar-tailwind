e2bcdf0129f892858c8d8835953ae018ae3ec337
Daud Kresna <daudkresna@gmail.com>
2024-02-29 14:36:53 +0700

    Customer Reviews

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,56 @@
-
 //Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu')
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function() {
-
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
+// Button Review
+// function btnClick() {
+//   const btn1 = document.querySelector('#btn1');
+//   btn1.classList.toggle('button-active');
+// };
+
 //Fixed Navbar
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
 };
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("review");
+  var btn = document.getElementsByClassName("btn-review");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+
+  for (i = 0; i < x.length; i++) {
+    
+    x[i].classList.add("hidden");
+  }
+  for (i = 0; i < x.length; i++) {
+    btn[i].classList.remove("scale-150");
+  }
+
+  x[slideIndex - 1].classList.remove("hidden");
+  x[slideIndex - 1].classList.add("block");
+  btn[slideIndex - 1].classList.add("scale-150");
+}
